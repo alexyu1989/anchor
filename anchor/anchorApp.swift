690bbf6ea47f1ba77760a21b65ae403ccbe443e3
Alex Yu <alexyu.dc@gmail.com>
2025-10-30 06:33:13 +0800

    Initial Commit

---
+++ anchor/anchorApp.swift
@@ -0,0 +1,17 @@
+//
+//  anchorApp.swift
+//  anchor
+//
+//  Created by Alex Yu on 2025/10/30.
+//
+
+import SwiftUI
+
+@main
+struct anchorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
